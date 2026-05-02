f9fd5bcf57e1387709cc714957135a58b6a94970
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-05-02 19:09:57 +0300

    Delete Backend directory

--- Backend/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    ROLE_CHOICES = [
-        ('client', 'Client'),
-        ('doctor', 'Doctor'),
-    ]
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
