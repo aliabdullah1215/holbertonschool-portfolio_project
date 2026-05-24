07b8897ec197450e80d0ffd1823cf234f76d1c76
Mohammed Basuliman <101177180+oDoDyK@users.noreply.github.com>
2026-05-24 22:31:27 +0300

    Update AppRoutes.jsx

--- Frontend/routes/AppRoutes.jsx
+++ Frontend/routes/AppRoutes.jsx
@@ -5,7 +5,6 @@ import RegisterScreen from '../pages/auth/RegisterScreen';
 import ClientAboutPage from '../pages/client/ClientAboutPage';
 import ClientAiPlansPage from '../pages/client/ClientAiPlansPage';
 import ClientAssessmentToolsPage from '../pages/client/ClientAssessmentToolsPage';
-import ClientContactPage from '../pages/client/ClientContactPage';
 import ClientHomePage from '../pages/client/ClientHomePage';
 import ClientMedicalSupportPage from '../pages/client/ClientMedicalSupportPage';
 import ClientPlansHistoryPage from '../pages/client/ClientPlansHistoryPage';
@@ -17,24 +16,76 @@ import DoctorJoinPage from '../pages/doctor/DoctorJoinPage';
 import DoctorShellPage from '../pages/doctor/DoctorShellPage';
 import LandingPage from '../pages/shared/LandingPage';
 import TeamPage from '../pages/shared/TeamPage';
+import HomeContactPage from '../pages/shared/HomeContactPage';
 import GuestRoute from './GuestRoute';
 import ProtectedRoute from './ProtectedRoute';
+import ScrollToTop from '../components/ScrollToTop';
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="client/contact" element={<HomeContactPage />} />
 
-      <Route
-        element={
-          <GuestRoute>
-            <AuthLayout />
-          </GuestRoute>
-        }
-      >
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
-      </Route>
+        {/* Guest Routes (Auth) */}
+        <Route
+          element={
+            <GuestRoute>
+              <AuthLayout />
+            </GuestRoute>
+          }
+        >
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
+        </Route>
+
+        {/* Protected Client Routes */}
+        <Route
+          path="/client"
+          element={
+            <ProtectedRoute allowedRole="client">
+              <ClientShellPage />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<ClientHomePage />} />
+          <Route path="assessment-tools" element={<ClientAssessmentToolsPage />} />
+          <Route path="plans-history" element={<ClientPlansHistoryPage />} />
+          <Route path="ai-plans" element={<ClientAiPlansPage />} />
+          <Route path="medical-support" element={<ClientMedicalSupportPage />} />
+          <Route path="about" element={<ClientAboutPage />} />
+          <Route path="team" element={<TeamPage />} />
+        </Route>
+
+        {/* Protected Doctor Routes */}
+        <Route
+          path="/doctor"
+          element={
+            <ProtectedRoute allowedRole="doctor">
+              <DoctorShellPage />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<DoctorHomePage />} />
+          <Route path="join" element={<DoctorJoinPage />} />
+          <Route path="contact" element={<DoctorContactPage />} />
+          <Route path="about" element={<DoctorAboutPage />} />
+          <Route path="team" element={<TeamPage />} />
+        </Route>
+
+        {/* Fallback Catch-All Route */}
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
+  );
+}
+
+export default AppRoutes;      </Route>
 
       <Route
         path="/client"
