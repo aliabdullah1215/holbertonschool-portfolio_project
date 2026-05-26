7bccd543eeee8becafee2c36e6b19be057a47127
Mohammed Basuliman <101177180+oDoDyK@users.noreply.github.com>
2026-05-26 08:26:24 +0300

    Update AppRoutes.jsx

--- Frontend/routes/AppRoutes.jsx
+++ Frontend/routes/AppRoutes.jsx
@@ -85,46 +85,4 @@ function AppRoutes() {
   );
 }
 
-export default AppRoutes;      </Route>
-
-      <Route
-        path="/client"
-        element={
-          <ProtectedRoute allowedRole="client">
-            <ClientShellPage />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<Navigate replace to="home" />} />
-        <Route path="home" element={<ClientHomePage />} />
-        <Route path="assessment-tools" element={<ClientAssessmentToolsPage />} />
-        <Route path="plans-history" element={<ClientPlansHistoryPage />} />
-        <Route path="ai-plans" element={<ClientAiPlansPage />} />
-        <Route path="medical-support" element={<ClientMedicalSupportPage />} />
-        <Route path="contact" element={<ClientContactPage />} />
-        <Route path="about" element={<ClientAboutPage />} />
-        <Route path="team" element={<TeamPage />} />
-      </Route>
-
-      <Route
-        path="/doctor"
-        element={
-          <ProtectedRoute allowedRole="doctor">
-            <DoctorShellPage />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<Navigate replace to="home" />} />
-        <Route path="home" element={<DoctorHomePage />} />
-        <Route path="join" element={<DoctorJoinPage />} />
-        <Route path="contact" element={<DoctorContactPage />} />
-        <Route path="about" element={<DoctorAboutPage />} />
-        <Route path="team" element={<TeamPage />} />
-      </Route>
-
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
-  );
-}
-
 export default AppRoutes;
